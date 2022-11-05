83e74887db3793d5901f263e3f8867fe29ce1f17
Elo <eloissemolina@gmail.com>
2022-11-05 13:56:54 -0600

    changes

--- app.py
+++ app.py
@@ -12,31 +12,21 @@ from helpers import login_required, session_activate
 load_dotenv()
 app = Flask(__name__)
 
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
-
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"),pool_size=10, max_overflow=20)
+engine = create_engine("postgresql://evavjxiahmilyp:bb12e5c80b49f5771b645e5fbb7152b6c3a838d65ba3e2e147ff58cef19e56cd@ec2-44-208-88-195.compute-1.amazonaws.com:5432/dagkhp34ch9ci8")
 db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/", methods=["GET"])
 @login_required
 def index():
-    return "peito"
+    return render_template("welcome.html")
     
-@app.route("/login", methods=["GET"])
-@session_activate
-def login():
-    if request.method == "GET":
-        return render_template("login.html")
-    
-'''@app.route("/index", methods=["POST"])
+@app.route("/index", methods=["POST"])
 def indice():
     return jsonify({"message": session["user_id"]})
 
@@ -192,4 +182,4 @@ def hola(isbn):
         else:
              db.execute("INSERT INTO reviews(review, stars, id_usuario, ISBN) VALUES(:review, :stars, :id_usuario,:ISBN)", {"review":entrada, "stars": stars, "id_usuario": id_clients, "ISBN": isbn})
              db.commit()
-             return jsonify({"mensaje":"subido" })'''
+             return jsonify({"mensaje":"subido" })
